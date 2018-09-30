8e7222c07b80577a12ff1be8eee5a81ccfd40fb6
fvictoria <felipevictoria630@yahoo.com>
2018-09-30 15:47:01 -0400

    ?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,33 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Enter string");
         Scanner kbReader = new Scanner(System.in);
-        String a = kbReader.nextLine();
-        
+        String input = kbReader.nextLine();
+        int length = input.length();
+        double RandomPercent = (int)(Math.random()*input.length());
+
+        String encryption;
+        if (RandomPercent == 1){
+        String encryption = Integer.toBinaryString();
+        }
+        else if(RandomPercent ==2){
+            String encryption= Integer.toOctalString();
+        }
+        else if(RandomPercent ==3){
+            String encryption = Integer.toDeciamlString();
+        }
+        else if(RandomPercent ==4){
+            String encryption = Integer.toHexString();
+        }
+        else if(RandomPercent ==5){
+            String encryption = Integer.toAsciiString();
+        }
+        System.out.println(encryption);
+
+
+
+
+
+
 
 
     }
